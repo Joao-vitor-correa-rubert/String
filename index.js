a7dc566e42817e6e7b1a40292472e08d91f0a31f
João Vitor <joao.rubertcorrea@gmail.com>
2022-01-12 16:55:44 -0300

    Strings

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let nome = "João"; //tipo primitivo
+let sobrenome = "Vitor";
+
+//let nome = new String("João");  // Tipo String não primitivo
+//let sobrenome = new String ("Vitor"); 
+
+console.log(nome+sobrenome); //concatenado sem espaço
+console.log(nome+" "+sobrenome);//concatenado com espaço 
+console.log("\"");// imprimir aspas sem erro
+console.log(nome+"\n"+sobrenome);//pulando linha
+console.log(`${nome} ${sobrenome}`);//concatenação com espaço nova atualização          
+console.log(`${nome} 
+${sobrenome}`);//concatenação atualizada pulando linha   
